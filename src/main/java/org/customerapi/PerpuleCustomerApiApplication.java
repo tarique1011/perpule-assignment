05a03537339e1c7eeba446a70ff4bf77c500efa4
Tarique Javaid <tarique.javaid11@gmail.com>
2019-04-25 03:01:57 +0530

    Initial commit

---
+++ src/main/java/org/customerapi/PerpuleCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package org.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpuleCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpuleCustomerApiApplication.class, args);
+	}
+
+}
